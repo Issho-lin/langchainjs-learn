780ad41f4a8f653b0a35903580f2505bb43e6bde
GEMDALE\linqibin <linqibin@gemdale.com>
2025-04-29 17:28:20 +0800

    Memory记忆能力

---
+++ documents/data.js
@@ -0,0 +1,13 @@
+function myFunction(name,job){
+	console.log("Welcome " + name + ", the " + job);
+}
+
+myFunction('Harry Potter','Wizard')
+
+function forFunction(){
+	for (let i=0; i<5; i++){
+        console.log("这个数字是" + i)
+	}
+}
+
+forFunction()
