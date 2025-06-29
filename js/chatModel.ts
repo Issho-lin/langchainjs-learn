4e5576784dc633c4f141976fd4d9c387acb512ba
GEMDALE\linqibin <linqibin@gemdale.com>
2025-06-29 13:03:14 +0800

    python版本loader

---
+++ js/chatModel.ts
@@ -0,0 +1,5 @@
+// import { AzureChatOpenAI } from '@langchain/openai'
+import { ChatTencentHunyuan } from '@langchain/community/chat_models/tencent_hunyuan';
+import { ChatAlibabaTongyi } from '@langchain/community/chat_models/alibaba_tongyi'
+
+console.log(ChatTencentHunyuan, ChatAlibabaTongyi)
---
+++ node/test.ts
@@ -0,0 +1,13 @@
+/*
+ * @Author: linqibin
+ * @Date: 2025-06-29 08:54:39
+ * @LastEditors: linqibin
+ * @LastEditTime: 2025-06-29 09:33:54
+ * @Description: 
+ * 
+ * Copyright (c) 2025 by linqibin@https://github.com/Issho-lin, All Rights Reserved. 
+ */
+import { ChatTencentHunyuan } from '@langchain/community/chat_models/tencent_hunyuan';
+import { AzureChatOpenAI } from '@langchain/openai'
+
+console.log(AzureChatOpenAI)
