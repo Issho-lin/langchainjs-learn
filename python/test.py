818821c6fc261f4c8342811324bdc86786353a0b
linqibin2 <linqibin2@gemdale.com>
2026-05-07 18:02:48 +0800

    feat: 添加RAG相关

--- python/test.py
+++ python/test.py
@@ -1,10 +1,18 @@
-def my_function(name,job): 
-	print("Welcome " + name + ", the " + job);
+'''
+Author: linqibin
+Date: 2025-06-30 08:58:01
+LastEditors: linqibin
+LastEditTime: 2025-09-04 17:51:59
+Description: 
 
-my_function(name='Harry Potter',job='Wizard')
+Copyright (c) 2025 by 智慧空间研究院/金地空间科技, All Rights Reserved. 
+'''
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_experimental.text_splitter import SemanticChunker
+from pprint import pprint
 
-def for_function():
-    for i in range(5):
-        print("这个数字是" + str(i))
+loader = PyPDFLoader(file_path='../documents/data.pdf', mode='page')
+rst = loader.load()
+for r in rst:
+    pprint(r.page_content)
 
-for_function()
